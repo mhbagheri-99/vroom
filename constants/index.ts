c0c9462ae35deb71e226cd7e0680ef38ce414e3e
mhbagheri-99 <mhbagheri3@gmail.com>
2024-04-16 18:00:14 +0200

    Initial Setup, Routing & Sidebar Component design

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+  {
+    label: "Home",
+    route: "/",
+    imageURL: "/icons/home.svg",
+  },
+  {
+    label: "Upcoming",
+    route: "/upcoming",
+    imageURL: "/icons/upcoming.svg",
+  },
+  {
+    label: "Previous",
+    route: "/previous",
+    imageURL: "/icons/previous.svg",
+  },
+  {
+    label: "Recordings",
+    route: "/recordings",
+    imageURL: "/icons/Video.svg",
+  },
+  {
+    label: "Personal Room",
+    route: "/personal-room",
+    imageURL: "/icons/add-personal.svg",
+  },
+];
